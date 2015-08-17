dcaf85f2b79ffd65c496c028071e1db6d1ffd78b
Vov4yk <krukvolod@gmail.com>
2015-08-17 17:07:48 +0300

    initial MoveController

--- src/io/x0x/controllers/MoveCotroller.java
+++ src/io/x0x/controllers/MoveCotroller.java
@@ -1,8 +1,18 @@
 package io.x0x.controllers;
 
-/**
- * Created by Vov4yk on 17.08.2015.
- */
+import io.x0x.model.Field;
+import io.x0x.model.Figure;
+import io.x0x.model.exceptions.AlreadyOccupiedException;
+import io.x0x.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+
 public class MoveCotroller {
-    public void applay
+    public void applyFigure(final Point point, final Field field, final Figure figure) throws InvalidPointException, AlreadyOccupiedException{
+        if (field.getFigure(point)!=null){
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point,figure);
+    }
 }
