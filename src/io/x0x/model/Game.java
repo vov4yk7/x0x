7a50b459b62f3bc0083f79b4bed43381d19340a1
Vov4yk <krukvolod@gmail.com>
2015-08-17 11:13:07 +0300

    add game

---
+++ src/io/x0x/model/Game.java
@@ -0,0 +1,28 @@
+package io.x0x.model;
+
+/**
+ * Created by Vov4yk on 17.08.2015.
+ */
+public class Game {
+    private final Player[] players;
+    private  final Field field;
+    private final String name;
+
+    public Game(final Player[] players, final Field field, final String name) {
+        this.players = players;
+        this.field = field;
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
