fe24b62569f2a3daf323999494a3e71a70f1caa7
Vov4yk <krukvolod@gmail.com>
2015-08-18 17:07:30 +0300

    refactor Field

--- src/io/x0x/model/Field.java
+++ src/io/x0x/model/Field.java
@@ -7,14 +7,18 @@ import java.awt.*;
 
 
 public class Field {
-    private  static final int FIELD_SIZE = 3;
     private  static final int MIN_COORDINATE = 0;
-    private  static final int MAX_COORDINATE = FIELD_SIZE;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private final Figure[][] field;
+    private  final int fieldSize;
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        this.field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize(){
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
@@ -33,10 +37,10 @@ public class Field {
     }
 
     private boolean checkPoint(final Point point){
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x,field.length) && checkCoordinate(point.y,field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate){
-        return  coordinate>=MIN_COORDINATE && coordinate<=MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate){
+        return  coordinate>=MIN_COORDINATE && coordinate<=maxCoordinate;
     }
 }
